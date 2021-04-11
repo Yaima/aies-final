60a8152454bb4c8af92db390f9ad582b4dea267e
Yaima <yaimamvaldivia@gmail.com>
2021-04-10 21:51:33 -0700

    Minor changes
    - Added dataset

--- aies-final.py
+++ aies-final.py
@@ -5,24 +5,26 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('Agg')
+
 import seaborn as sns
 from scipy.stats import pearsonr
 from itertools import cycle
 
 
 def STEP1():
-    df = pd.read_csv(r"/Users/Yaima/Documents/Python/AIES/toxity_per_attribute.csv", delimiter=',')
-
-    print("Number of records: ")
+    df = pd.read_csv('dataset/credit_card_csv.csv', delimiter=',')
+    print(df.head(5))
+    print("Number of records: ", len(df))
     r, c = df.shape
     print("rows: ", r)
     print("columns: ", c)
     print()
+    for col in df.columns:
+        print(col)
+
 
 def STEP2():
-    df = pd.read_csv(r"/Users/Yaima/Documents/Python/AIES/toxity_per_attribute.csv", delimiter=',')
-
+    df = pd.read_csv(r"dataset/", delimiter=',')
     print("Number of records: ")
     r, c = df.shape
     print("rows: ", r)
@@ -31,26 +33,27 @@ def STEP2():
 
 
 def STEP3():
-    df = pd.read_csv(r"/Users/Yaima/Documents/Python/AIES/toxity_per_attribute.csv", delimiter=',')
+    df = pd.read_csv(r"dataset/", delimiter=',')
 
     print("Number of records: ")
     r, c = df.shape
     print("rows: ", r)
     print("columns: ", c)
     print()
+
 
 def STEP4():
-    df = pd.read_csv(r"/Users/Yaima/Documents/Python/AIES/toxity_per_attribute.csv", delimiter=',')
+    df = pd.read_csv(r"dataset/", delimiter=',')
 
     print("Number of records: ")
     r, c = df.shape
     print("rows: ", r)
     print("columns: ", c)
     print()
+
 
 if __name__ == '__main__':
     STEP1()
-    STEP2()
-    STEP3()
-    STEP4()
-    STEP5()
+    # STEP2()
+    # STEP3()
+    # STEP4()
