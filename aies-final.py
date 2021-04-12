515c16b91d8a7197545d71de76b21286aa1381b2
Yaima <yaimamvaldivia@gmail.com>
2021-04-11 21:25:51 -0700

    New dataset
    Education

--- aies-final.py
+++ aies-final.py
@@ -12,15 +12,37 @@ from itertools import cycle
 
 
 def STEP1():
-    df = pd.read_csv('dataset/credit_card_csv.csv', delimiter=',')
-    print(df.head(5))
-    print("Number of records: ", len(df))
-    r, c = df.shape
-    print("rows: ", r)
-    print("columns: ", c)
+    df_por = pd.read_csv('dataset/student-por.csv', delimiter=';')
+    print(df_por.head(5))
+    print("Number of records port: ", len(df_por))
+    r, c = df_por.shape
+    print("rows port: ", r)
+    print("columns port: ", c)
     print()
-    for col in df.columns:
+    for col in df_por.columns:
         print(col)
+
+    df_mat = pd.read_csv('dataset/student-mat.csv', delimiter=';')
+    print(df_mat.head(5))
+    print("Number of records math: ", len(df_mat))
+    r, c = df_mat.shape
+    print("rows math: ", r)
+    print("columns math: ", c)
+    print()
+    for col in df_mat.columns:
+        print(col)
+
+    merged = [df_por, df_mat]
+    merged = pd.concat(merged)
+    print(merged.head(5))
+    print("Number of records merged: ", len(merged))
+    r, c = merged.shape
+    print("rows merged: ", r)
+    print("columns merged: ", c)
+    print()
+    for col in merged.columns:
+        print(col)
+    merged.to_csv(r'out/merged.csv')
 
 
 def STEP2():
