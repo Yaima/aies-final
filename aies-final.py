1f5c61cdb2de10ba0608cec9566c8e8c5d311be0
Yaima <yaimamvaldivia@gmail.com>
2021-04-11 21:33:21 -0700

    No need to merge
    Commenting out the merge code, we can use the Portuguese Dataset.

--- aies-final.py
+++ aies-final.py
@@ -22,27 +22,27 @@ def STEP1():
     for col in df_por.columns:
         print(col)
 
-    df_mat = pd.read_csv('dataset/student-mat.csv', delimiter=';')
-    print(df_mat.head(5))
-    print("Number of records math: ", len(df_mat))
-    r, c = df_mat.shape
-    print("rows math: ", r)
-    print("columns math: ", c)
-    print()
-    for col in df_mat.columns:
-        print(col)
-
-    merged = [df_por, df_mat]
-    merged = pd.concat(merged)
-    print(merged.head(5))
-    print("Number of records merged: ", len(merged))
-    r, c = merged.shape
-    print("rows merged: ", r)
-    print("columns merged: ", c)
-    print()
-    for col in merged.columns:
-        print(col)
-    merged.to_csv(r'out/merged.csv')
+    # df_mat = pd.read_csv('dataset/student-mat.csv', delimiter=';')
+    # print(df_mat.head(5))
+    # print("Number of records math: ", len(df_mat))
+    # r, c = df_mat.shape
+    # print("rows math: ", r)
+    # print("columns math: ", c)
+    # print()
+    # for col in df_mat.columns:
+    #     print(col)
+    #
+    # merged = [df_por, df_mat]
+    # merged = pd.concat(merged)
+    # print(merged.head(5))
+    # print("Number of records merged: ", len(merged))
+    # r, c = merged.shape
+    # print("rows merged: ", r)
+    # print("columns merged: ", c)
+    # print()
+    # for col in merged.columns:
+    #     print(col)
+    # merged.to_csv(r'out/merged.csv')
 
 
 def STEP2():
